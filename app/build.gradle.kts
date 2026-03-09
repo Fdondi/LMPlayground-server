b04087285b9cd33680642db37d7f60a1e0599abf
Andrew Druk <andriy.druk@gmail.com>
2026-03-09 15:49:34 +0200

    System: bump version to 1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.druk.lmplayground"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1_02_01
-        versionName = "1.2.1"
+        versionCode = 1_02_02
+        versionName = "1.2.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
 
