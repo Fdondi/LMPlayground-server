e4697496dfe1bcc98940c6ec97c908c5f1eac958
Andrew Druk <andriy.druk@gmail.com>
2026-04-13 14:56:12 +0300

    System: bump version to 1.4.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.druk.lmplayground"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1_04_04
-        versionName = "1.4.4"
+        versionCode = 1_04_05
+        versionName = "1.4.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
 
